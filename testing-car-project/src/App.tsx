55711801c25d52f6667d66f82bc9b494e9ff1e19
jessie pan <jessie.pan@autotrader.co.uk>
2024-07-01 16:08:05 +0100

    adding the basic inf

--- testing-car-project/src/App.tsx
+++ testing-car-project/src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function App() {
-  return <p>this is testing</p>;
+  return (
+    <>
+      <header>
+        <h1>Find your next car</h1>
+        <p>Search new and used cars by make and model.</p>
+      </header>
+      <section>
+        <form>
+          <fieldset>
+            <legend className="visuallyhidden">Search filters</legend>
+            <label>Make</label>
+            <select>
+              <option value="">Makes</option>
+            </select>
+          </fieldset>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default App;
